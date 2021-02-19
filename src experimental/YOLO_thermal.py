0086ebd283b1d73bb6ad39a6f1cea8220ae22f78
vdBerg93 <anonymous@a.com>
2021-02-19 11:03:41 +0100

    Added berend print function for testing

--- src experimental/YOLO_thermal.py
+++ src experimental/YOLO_thermal.py
@@ -5,9 +5,11 @@ import pandas as pd
 from localFunctions import find_digits, find_objects_and_write, read_and_display_boxes, resize_image,create_archive
 
 print('Test for github')
+print("Berend can also run this!")
+
 # Image numbers
 start = 0
-end = 200
+end = start+200
 steps = 50
 
 #desired height and weight for resizing the image
