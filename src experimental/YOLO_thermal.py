2ffe3fb46b46e023ed54bf54617c44b916d07e81
A-Moayyedi <78637873+A-Moayyedi@users.noreply.github.com>
2021-02-19 09:56:35 +0100

    FIX MESS
    20210219 Trying to fix my mess -Ali

--- src experimental/YOLO_thermal.py
+++ src experimental/YOLO_thermal.py
@@ -1,12 +1,20 @@
 import cv2
 import numpy as np
 import pandas as pd
-from localFunctions import find_digits, find_objects_and_write, read_and_display_boxes
+from localFunctions import find_digits, find_objects_and_write, read_and_display_boxes, resize_image,create_archive
+
 
 # Image numbers
 start = 0
-end = 0
-steps = 1
+end = 200
+steps = 50
+
+#desired height and weight for resizing the image
+desired_height=400
+desired_width=600
+
+#Creating an archive df
+df_whole=create_archive('Yolo_config/coco.names')
 
 # Look into folders and choose
 dataset = "V0"  # V0 - day, V1 - night
@@ -16,30 +24,46 @@ file_type = [".jpg",".csv"]
 i = start
 while i <= end:
     d4,d3,d2,d1 = find_digits(i)
-    path_rgb = "Dataset_"+dataset+"/images/set00/"+subdataset+"/visible/I0"+str(d4)+str(d3)+str(d2)+str(d1)
-    path_thermal = "Dataset_" + dataset + "/images/set00/" + subdataset + "/thermal/I0" + str(d4) + str(d3) + str(d2) + str(d1)
+    path_rgb = "Data/Dataset_"+dataset+"/images/set00/"+subdataset+"/visible/I0"+str(d4)+str(d3)+str(d2)+str(d1)
+    path_thermal = "Data/Dataset_" + dataset + "/images/set00/" + subdataset + "/thermal/I0" + str(d4) + str(d3) + str(d2) + str(d1)
     i += steps
 
-    img_rgb = cv2.imread(path_rgb+file_type[0])
-    img_thermal = cv2.imread(path_thermal+file_type[0])
+    #Giving as input original images to cv2
+    # img_rgb = cv2.imread(path_rgb+file_type[0])
+    # img_thermal = cv2.imread(path_thermal+file_type[0])
+
+    #Giving a path for saving resized rgb and thermal images with a directory similar to original ones but in a _resized folder
+    path_rgb_resized = "Data/Dataset_" + dataset + "_resized/images/set00/" + subdataset + "/visible/I0" + str(d4) + str(d3) + str(d2) + str(
+        d1) + "_resized"
+    path_thermal_resized = "Data/Dataset_" + dataset + "_resized/images/set00/" + subdataset + "/thermal/I0" + str(d4) + str(d3) + str(
+        d2) + str(d1) + "_resized"
+    #Calling the resize_image functions that takes the original images' paths and file type and the desired width and height
+    #and path for saving the resized pictures
+    resize_image(path_rgb,file_type[0],desired_width,desired_height,path_rgb_resized)
+    resize_image(path_thermal,file_type[0],desired_width,desired_height,path_thermal_resized)
+    #giving as input resized images to cv2
+    img_rgb = cv2.imread(path_rgb_resized+file_type[0])
+    img_thermal = cv2.imread(path_thermal_resized+file_type[0])
+
 
     whT = 320
     confThreshold = 0.5
     nmsThreshold = 0.3  # lower ==> less number of boxes
 
-    classesFile = 'coco.names'
+    classesFile = 'Yolo_config/coco.names'
     classNames = []
     with open(classesFile, 'r') as f:                       # using WITH function takes away the need to use CLOSE file function
         classNames = f.read().rstrip('\n').split('\n')      # rstrip strips off the ("content here") and split splits off for("content here")
 
-    modelConfiguration = 'yolov3.cfg'
-    modelWeights = 'yolov3.weights'
+    modelConfiguration = 'Yolo_config/yolov3.cfg'
+    modelWeights = 'Yolo_config/yolov3.weights'
 
     net = cv2.dnn.readNetFromDarknet(modelConfiguration,modelWeights)
     net.setPreferableBackend(cv2.dnn.DNN_BACKEND_OPENCV)
     net.setPreferableTarget(cv2.dnn.DNN_TARGET_CPU)
 
     blob = cv2.dnn.blobFromImage(img_rgb, 1/255,(whT, whT), [0,0,0],1,False)
+
     net.setInput(blob)
 
     layerNames = net.getLayerNames()
@@ -47,9 +71,17 @@ while i <= end:
 
     outputs = net.forward(outputNames)
 
-    # find_objects_and_write(outputs, img_rgb, img_thermal, classNames, confThreshold, nmsThreshold, path_thermal, file_type[1])
-    # cv2.imshow("Color ", img_rgb)
-    # cv2.imshow("Thermal (corresponding) ", img_thermal)
-    # cv2.waitKey(5)
+    df=find_objects_and_write(outputs, img_rgb, img_thermal, classNames, confThreshold, nmsThreshold, path_thermal, file_type[1],df_whole)
 
-    read_and_display_boxes(path_thermal)
+    find_objects_and_write(outputs, img_rgb, img_thermal, classNames, confThreshold, nmsThreshold, path_thermal_resized,
+                           file_type[1],df_whole)
+
+    df_whole.append(df, ignore_index=True)
+
+    cv2.imshow("Color ", img_rgb)
+    cv2.imshow("Thermal (corresponding) ", img_thermal)
+    cv2.waitKey(100) #miliseconds of pause between different pictures
+
+    #read_and_display_boxes(path_thermal)
+
+df_whole.to_csv('Archive.csv')
--- src experimental/localFunctions.py
+++ src experimental/localFunctions.py
@@ -1,6 +1,7 @@
 import numpy as np
 import cv2
 import pandas as pd
+from PIL import Image
 
 def find_digits(i):
     d1 = i
@@ -25,7 +26,7 @@ def find_digits(i):
     return d4,d3,d2,d1
 
 
-def find_objects_and_write(outputs, img_rgb, img_therm, classNames, confThreshold, nmsThreshold, path_thermal, file_type):
+def find_objects_and_write(outputs, img_rgb, img_therm, classNames, confThreshold, nmsThreshold, path_thermal, file_type, df_whole):
     hT, wT, cT = img_rgb.shape
     bbox = []
     classIds = []
@@ -54,6 +55,18 @@ def find_objects_and_write(outputs, img_rgb, img_therm, classNames, confThreshol
     df = pd.DataFrame(columns=CL)
     j = 0
 
+    #Under development for creating a global csv file containing all the images' results
+    # CL_whole=classNames.copy()
+    # CL_whole.insert(0, "Image")
+    # CL_whole.insert(1, "Box")
+    # CL_whole.insert(2, "xc")
+    # CL_whole.insert(3, "yc")
+    # CL_whole.insert(4, "w")
+    # CL_whole.insert(5, "h")
+    # df_whole=pd.DataFrame(columns=CL_whole)
+    # jj=0
+
+
     for i in indices:
         i = i[0]  # to squeeze the dimension
         box = bbox[i]
@@ -74,7 +87,28 @@ def find_objects_and_write(outputs, img_rgb, img_therm, classNames, confThreshol
         df[CL[4]][j] = h
         df[classNames[classIds[i]]][j] = confs[i]
         j += 1
+
+        #under development
+        # archive_results(path_thermal[-4:],jj,x,y,w,h,confs[i])
+        # df_whole = df_whole.append(pd.Series(0, index=df_whole.columns), ignore_index=True)
+        # df_whole[CL_whole[0]][j] = path_thermal[-4:]
+        # df_whole[CL_whole[1]][j] = jj+1
+        # df_whole[CL_whole[2]][j] = x
+        # df_whole[CL_whole[3]][j] = y
+        # df_whole[CL_whole[4]][j] = w
+        # df_whole[CL_whole[5]][j] = h
+        # df_whole[classNames[classIds[i]]][j] = confs[i]
+        # jj += 1
+
+
     df.to_csv(path_thermal + file_type, index=False)
+
+
+    #TEST
+    return df
+    # df_whole.append(df,ignore_index=True)
+    # df.to_csv('Archive.csv')
+
 
 
 def read_and_display_boxes(file_path):
@@ -89,3 +123,51 @@ def read_and_display_boxes(file_path):
                     (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 0, 255), 2)
     cv2.imshow("Thermal image with boxes ", img_thermal)
     cv2.waitKey(0)
+
+#function to take an input image, resize it according to the desired height and width and save it to a specific directory
+def resize_image(path,file_type,desired_width,desired_height,path_resized):
+    original_image = Image.open(path+file_type)
+    resized_image = original_image.resize((desired_width,desired_height), Image.ANTIALIAS)
+    resized_image.save(path_resized+file_type)
+    return resized_image
+
+#Function to save all the results in one file
+def create_archive(classesFileName):
+    classesFile = classesFileName #'coco.names'
+    classNames = []
+    with open(classesFile, 'r') as f:                       # using WITH function takes away the need to use CLOSE file function
+        classNames = f.read().rstrip('\n').split('\n')      # rstrip strips off the ("content here") and split splits off for("content here")
+    CL_whole = classNames.copy()
+    CL_whole.insert(0, "Image")
+    CL_whole.insert(1, "Box")
+    CL_whole.insert(2, "xc")
+    CL_whole.insert(3, "yc")
+    CL_whole.insert(4, "w")
+    CL_whole.insert(5, "h")
+    df_whole = pd.DataFrame(columns=CL_whole)
+    return df_whole
+
+    # if Archive == True:
+    #     CL_whole = classNames.copy()
+    #     CL_whole.insert(0, "Image")
+    #     CL_whole.insert(1, "Box")
+    #     CL_whole.insert(2, "xc")
+    #     CL_whole.insert(3, "yc")
+    #     CL_whole.insert(4, "w")
+    #     CL_whole.insert(5, "h")
+    #     df_whole = pd.DataFrame(columns=CL_whole)
+    #     return df_whole
+
+# def archive_results(df_whole,df):
+#     #df_whole = df_whole.append(pd.Series(0, index=df_whole.columns), ignore_index=True)
+#     df_whole.append(df,ignore_index=True)
+
+
+    # df_whole[CL_whole[0]][j] = path_thermal[-4:]
+    # df_whole[CL_whole[1]][j] = jj+1
+    # df_whole[CL_whole[2]][j] = x
+    # df_whole[CL_whole[3]][j] = y
+    # df_whole[CL_whole[4]][j] = w
+    # df_whole[CL_whole[5]][j] = h
+    # df_whole[classNames[classIds[i]]][j] = confs[i]
+    # jj += 1
