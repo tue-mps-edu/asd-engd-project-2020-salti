6851dcd8f69436e04fa01b4bd071dd74919623f5
Markos0896 <78594188+Markos0896@users.noreply.github.com>
2021-02-23 12:09:09 +0100

    Updated UserValidation function

--- src experimental/Validation.py
+++ src experimental/Validation.py
@@ -1,17 +1,22 @@
-
-def UserValidation(variables):
+#file_path = "Data\Dataset_V0\images\set00\V000\YOLO-GUI"
+#filename_yolo = "I00000"
+#filename_gui = "GOPR0376_frame_000001_rgb_anon"
+def UserValidation(file_path, filname_yolo, filename_gui):
     import pandas as pd
     import os
     #this is code for reading output files from YOlO and from the GUI
     #YOLO
 
-    #GUI
-
-
-    a = pd.read_csv("GOPR0376_frame_000001_rgb_anon.txt", delim_whitespace=True,
+    a = pd.read_csv(file_path + "/" + filname_yolo + ".csv", delim_whitespace=True,
                     names=["classes", "x_c", "ÿ_c", "w", "h"])
-    a['filename'] = os.path.basename("GOPR0376_frame_000001_rgb_anon.txt")
-    # pd.DataFrame("GOPR0376_frame_000001_rgb_anon")
-
+    a['filename'] = os.path.basename(file_path+ "/" + filname_yolo + ".csv")
     print(a)
 
+    #GUI
+
+    b = pd.read_csv(file_path + "/"+ filename_gui + ".txt", delim_whitespace=True,
+                    names=["classes", "x_c", "ÿ_c", "w", "h"])
+    b['filename'] = os.path.basename(file_path + "/"+ filename_gui + ".txt")
+    print(b)
+
+#UserValidation(file_path, filename_yolo,filename_gui)
