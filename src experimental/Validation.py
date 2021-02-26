f3d848c6eea9c1eaa4f01108c7e2cb1b19a8627e
A-Moayyedi <78637873+A-Moayyedi@users.noreply.github.com>
2021-02-26 12:11:21 +0100

    Validation Completed
    TP, FP, FN, accuracy, recall, precision ready

--- src experimental/Validation.py
+++ src experimental/Validation.py
@@ -8,66 +8,64 @@ import os
 def UserValidation(file_path, filename_yolo, filename_gui):
     #this is code for reading output files from YOlO and from the GUI
     #GUI
-
     GU = pd.read_csv(os.path.join(file_path,filename_gui + ".txt"), delim_whitespace=True,
                     names=["classes", "x_c", "y_c", "w", "h"])
-    #a['filename'] = os.path.basename(file_path + "/" + filename_gui + ".txt")
     GU['filename'] = filename_gui
-    print(GU)
 
     #Yolo
     YO = pd.read_csv(os.path.join(file_path,filename_yolo + ".txt"), delim_whitespace=True,
                     names=["classes", "x_c", "y_c", "w", "h"])
     YO['filename'] = filename_yolo
-    print(YO)
-
-
-    # maths:
-    #print(a.loc[2][1])
-    #print(a['x_c'][0])
 
     TP=0
     FP=0
     FN=0
+    distances_mat=np.zeros((GU.shape[0],YO.shape[0]))
 
     for index_GU in range(GU.shape[0]): #0 to 2 (along columns of gui)
-        distances = np.zeros(YO.shape[0])
         for index_YO in range(YO.shape[0]): # 0 to 3 (along columns of yolo)
-            #centroid_distances = np.add(a['x_c'][index_a], b['x_c'][index_b])
             centroid_distances = np.sqrt( (GU['x_c'][index_GU]-YO['x_c'][index_YO])**2 + (GU['y_c'][index_GU]-YO['y_c'][index_YO])**2)
-            distances[index_YO] = centroid_distances
-        print('Distances of a {} from YO are {}'.format(index_GU,distances))
+            distances_mat[index_GU][index_YO] = centroid_distances
+    #print('Distances matrix of size {} is: \n {}'.format(distances_mat.shape,distances_mat))
+
+    #Creating a dataframe for distances between GUI (in rows) boxes and Yolo (in column) boxes
+    distances_df=pd.DataFrame(data=distances_mat)
 
 
+    while distances_df.shape[0] >= 1 and distances_df.shape[1] >= 1:
+        # To find the indexes of global minimum inside the dataframe
+        GUI_min_ind = distances_df.min(axis=1).idxmin()
+        YO_min_ind = distances_df.min().idxmin()
 
-        #find the minimum
-        minElement = np.amin(distances)
-        ind = np.argmin(distances)
-        #print('The min distance between GU {} from YO is {} at index {}'.format(index_GU,minElement,ind))
 
         #compare the minimum with diameter of the larger box
-        box1 = np.sqrt( (GU['w'][index_GU]) **2 + (GU['h'][index_GU]) **2 )
-        #print(f'this is diameter of the GUI box {box1}')
-        box2 = np.sqrt( (YO['w'][ind]) **2 + (YO['h'][ind]) **2 )
-        #print(f'this is diameter of the YOLO box {box2}')
-        bigger_diameter = max(box1, box2)
-        #print('the bigger diameter is {}'.format(bigger_diameter))
-        if minElement < bigger_diameter:
+        GUI_diam = np.sqrt( (GU['w'][GUI_min_ind]) **2 + (GU['h'][GUI_min_ind]) **2 )
+        YO_diam = np.sqrt((YO['w'][YO_min_ind]) ** 2 + (YO['h'][YO_min_ind]) ** 2)
+        bigger_diameter = max(GUI_diam, YO_diam)
+        if distances_df[GUI_min_ind][YO_min_ind] < bigger_diameter:
             #They are the same boxes
-            if GU['classes'][index_GU]==YO['classes'][ind]:
+            if GU['classes'][GUI_min_ind]==YO['classes'][YO_min_ind]:
                 TP+=1
             else:
                 FP+=1
-        else:
-            FN+=1
-    Precision = TP/(TP+FP)
-    Recall = TP/(TP + FN)
-    Accuracy = TP/(TP + FP + FN)
-    print('TP is equal to {}, FP is equal to {} and FN is equal to {}'. format(TP, FP, FN))
+
+        # To discard the analyzed row and column
+        distances_df = distances_df.drop(index=[GUI_min_ind], columns=[YO_min_ind])
+
+
+    FN+=distances_df.shape[0]
+    FP+=distances_df.shape[1]
+
+    print('True positive = {}'.format(TP))
+    print('False positive = {}'.format(FP))
+    print('False Negative = {}'.format(FN))
+    Precision = TP / (TP + FP)
+    Recall = TP / (TP + FN)
+    Accuracy = TP / (TP + FP + FN)
     print('Precision is {}, Recall is {} and Accuracy is {}'.format(Precision, Recall, Accuracy))
+
 UserValidation(dir_thermal_resized,'I00799_YOLO','I00799')
 
 
 #read_and_display_boxes(dir_Validation, 'I02199')
 
-print('Hello')
