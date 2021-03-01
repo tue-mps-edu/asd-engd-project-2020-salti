a5ac2e1d765c9e7178a9ed3e8ccdda3f5b5b58d4
vdBerg93 <anonymous@a.com>
2021-03-01 17:29:17 +0100

    change import image add initialization

--- SALTI src/detect_thermal.py
+++ SALTI src/detect_thermal.py
@@ -11,15 +11,114 @@ import sys
 from config import *
 
 def initialize():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--cfg', type=str, default=cfg_T.dir_cfg, help='cfg file path')
+    parser.add_argument('--data', type=str, default=cfg_T.dir_classes, help='coco.data file path')
+    parser.add_argument('--weights', type=str, default='Yolo_config\yolov3-thermal.weights', help='path to weights file')
+    parser.add_argument('--source', type=str, default=r'C:\Github\asd-pdeng-project-2020-developer\SALTI src\Data\mytestdata\RGB', help='source')  # input file/folder, 0 for webcam
+    parser.add_argument('--output', type=str, default='output', help='output folder')  # output folder
+    parser.add_argument('--img-size', type=int, default=416, help='inference size (pixels)')
+    parser.add_argument('--conf-thres', type=float, default=0.3, help='object confidence threshold')
+    parser.add_argument('--nms-thres', type=float, default=0.5, help='iou threshold for non-maximum suppression')
+    parser.add_argument('--fourcc', type=str, default='mp4v', help='output video codec (verify ffmpeg support)')
+    parser.add_argument('--half', action='store_true', help='half precision FP16 inference')
+    parser.add_argument('--device', default='', help='device id (i.e. 0 or 0,1) or cpu')
+    parser.add_argument('--view-img', action='store_true', help='display results')
+    opt = parser.parse_args()
+    print(opt)
+
+    with torch.no_grad():
+        img_size = (320, 192) if ONNX_EXPORT else opt.img_size  # (320, 192) or (416, 256) or (608, 352) for (height, width)
+        out, source, weights, half, view_img = opt.output, opt.source, opt.weights, opt.half, opt.view_img
+        webcam = source == '0' or source.startswith('rtsp') or source.startswith('http') or source.endswith('.txt')
+
+        # Initialize
+        device = torch_utils.select_device(device='cpu' if ONNX_EXPORT else opt.device)
+        if os.path.exists(out):
+            shutil.rmtree(out)  # delete output folder
+        os.makedirs(out)  # make new output folder
+
+        # Initialize model
+        model = Darknet(opt.cfg, img_size)
+
+        # Load weights
+        attempt_download(weights)
+        if weights.endswith('.pt'):  # pytorch format
+            model.load_state_dict(torch.load(weights, map_location=device)['model'])
+        else:  # darknet format
+            _ = load_darknet_weights(model, weights)
+
+        # Second-stage classifier
+        classify = False
+        if classify:
+            modelc = torch_utils.load_classifier(name='resnet101', n=2)  # initialize
+            modelc.load_state_dict(torch.load('weights/resnet101.pt', map_location=device)['model'])  # load weights
+            modelc.to(device).eval()
+
+        # Fuse Conv2d + BatchNorm2d layers
+        # model.fuse()
+
+        # Eval mode
+        model.to(device).eval()
+
+        # Export mode
+        if ONNX_EXPORT:
+            img = torch.zeros((1, 3) + img_size)  # (1, 3, 320, 192)
+            torch.onnx.export(model, img, 'weights/export.onnx', verbose=False, opset_version=11)
+
+            # Validate exported model
+            import onnx
+            model = onnx.load('weights/export.onnx')  # Load the ONNX model
+            onnx.checker.check_model(model)  # Check that the IR is well formed
+            print(onnx.helper.printable_graph(model.graph))  # Print a human readable representation of the graph
+            return
+
+        # Half precision
+        half = half and device.type != 'cpu'  # half precision only supported on CUDA
+        if half:
+            model.half()
+
+        # Set Dataloader
+        # vid_path, vid_writer = None, None
+        # if webcam:
+        #     view_img = True
+        #     torch.backends.cudnn.benchmark = True  # set True to speed up constant image size inference
+        #     dataset = LoadStreams(source, img_size=img_size, half=half)
+        # else:
+        #     save_img = True
+        #     dataset = LoadImages(source, img_size=img_size, half=half)
+
+        # Get classes and colors
+        classes = load_classes(parse_data_cfg(opt.data)['names'])
+#        colors = [[random.randint(0, 255) for _ in range(3)] for _ in range(len(classes))]
+
+    return model, classes, opt
+
+    #return model, classes
 
 
-    # return model
+def detect(model, img, opt):
+    # Run inference
+    t0 = time.time()
 
 
-def detect(model, image):
+    #t = time.time()
+
+    # Get detections
+    #img = torch.from_numpy(img).to(device)
+    print("check if image is right format")
+    if img.ndimension() == 3:
+        img = img.unsqueeze(0)
+    pred = model(img)[0]
+
+    if opt.half:
+        pred = pred.float()
+
+    # Apply NMS
+    pred = non_max_suppression(pred, opt.conf_thres, opt.nms_thres)
 
 
-    # return bbox, conf, etc
+    return pred
 
 
 def detect_old(save_txt=False, save_img=False):
@@ -215,4 +314,4 @@ def detect_old(save_txt=False, save_img=False):
 
         print('Done. (%.3fs)' % (time.time() - t0))
 
-detect_old()
+#detect_old()
