8eab281c275e814a7bebf8d7f18de62cee53bf7b
Khetan <y.khetan@tue.nl>
2021-03-02 16:05:44 +0100

    YoloNMS

--- nms_python-master/nms_python-master/nms_test.py
+++ nms_python-master/nms_python-master/nms_test.py
@@ -5,95 +5,60 @@ import cv2
 import time
 import xml.etree.ElementTree as ET
 import pandas as pd
+import csv
 
 dfr=pd.read_csv('test_excel.csv')
 dft=pd.read_csv('test_excel1.csv')
+dfc=dfr.append(dft)
+print(dfc)
 def merge(dfr,dft):
 ## Coordinates extraction
     lengthr=len(dfr.index)
     lengtht=len(dft.index)
-    i=1
     boxes=[]
     box=[]
     label=[]
     labels=[]
+    confidence=[]
+    i=0
     for i in range(lengthr):
-        x1=dfr.iloc[i,2]
-        y1=dfr.iloc[i,3]
-        x2 = dfr.iloc[i, 4]
-        y2 = dfr.iloc[i, 5]
-        boxr=[x1,y1,x2,y2]
+        x1=dfr.iloc[i,3]
+        y1=dfr.iloc[i,4]
+        x2 = dfr.iloc[i, 5]
+        y2 = dfr.iloc[i, 6]
+        conf=float(dfr.iloc[i,8])
+        boxr=[int((x1+x2)/2),int((y1+y2)/2),int(abs(y1-y2)),int(abs(x1-x2))]
         boxes=np.append(boxes,boxr,axis=0)
-
+        confidence=np.append(confidence,conf)
         labelr=[dfr.iloc[i, 6]]
         labels=np.append(labels,labelr,axis=0)
 
 
     for i in range(lengtht):
-        x1 = dft.iloc[i, 2]
-        y1 = dft.iloc[i, 3]
-        x2 = dft.iloc[i, 4]
-        y2 = dft.iloc[i, 5]
-        boxt = [x1, y1, x2, y2]
+        x1 = dft.iloc[i, 3]
+        y1 = dft.iloc[i, 4]
+        x2 = dft.iloc[i, 5]
+        y2 = dft.iloc[i, 6]
+        conf = float(dft.iloc[i, 8])
+        boxt=[int((x1+x2)/2),int((y1+y2)/2),int(abs(y1-y2)),int(abs(x1-x2))]
         boxes = np.append(boxes, boxt, axis=0)
-
+        confidence = np.append(confidence, conf)
         labelt = [dft.iloc[i, 6]]
         labels = np.append(labels, labelt, axis=0)
 
-    boxes= np.reshape(boxes,(-1,4))
     labels=np.reshape(labels,(-1,1))
-    boxes=boxes.astype(int)
-
-    images = [("images/Parking.jpg", boxes,labels)]
-    iter_num= 1
-    images = images*iter_num  # change the iterations to compare the two nms method
-
-    t1 = time.time()
-
-    # loop over the images
-    for (i, (imagePath, boundingBoxes,labels)) in enumerate(images):
-        # load the image and clone it
-        # print ("[x] %d initial bounding boxes" % (len(boundingBoxes)))
-        image = cv2.imread(imagePath)
-        orig = image.copy()
-
-        # loop over the bounding boxes for each image and draw them
-        for (startX, startY, endX, endY) in boundingBoxes:
-            cv2.rectangle(orig, (startX, startY), (endX, endY), (0, 0, 255), 2)
-
-        # perform non-maximum suppression on the bounding boxes
-        # pick = non_max_suppression_slow(boundingBoxes, 0.3)
-
-        pick,pick_labels = non_max_suppression_fast(boundingBoxes,labels, probs=None, overlapThresh=0.3)
-        # print ("[x] after applying non-maximum, %d bounding boxes" % (len(pick)))
-
-        # loop over the picked bounding boxes and draw them
-        for (startX, startY, endX, endY) in pick:
-            cv2.rectangle(image, (startX, startY), (endX, endY), (0, 255, 0), 2)
-
-        # display the images
-        # cv2.imshow("Original" + i, orig)
-        # cv2.imshow("After NMS" + i, image)
-        # cv2.waitKey(0)
-
-        # save the images
-        cv2.imwrite("images/Original_test" + str(i) + ".jpg", orig)
-        cv2.imwrite("images/After_NMS_test" + str(i) + ".jpg", image)
+    boxes=list(boxes)
+    confidence=list(confidence)
+    boxes=[boxes[x:x+4] for x in range(0, len(boxes), 4)]
 
 
-    return pick,pick_labels
+    return boxes,confidence
 
-box,label=merge(dfr,dft)
-print(box,label)
-# t2 = time.time()
-# print('cost {} ms to process {} images'.format((t2 - t1)*1000, len(images)))
-# boxes,labels = extract("test_excel.csv")
-# boxes1,labels1= extract("test_excel1.csv")
-# a=boxes
-# b=boxes1
-# bbox=np.append(a,b,axis=0)
-# bbox=bbox.astype(int)
-# print(type(bbox))
+boxes,confidence=merge(dfr,dft)
+print(boxes)
+yu= cv2.dnn.NMSBoxes(boxes, confidence, 0.1, 0.1)
+print(yu)
 
-# construct a list containing the images that will be examined
-# along with their respective bounding boxes
+dfc=dfc.drop(dfc.index[1])
+print(dfc)
+
