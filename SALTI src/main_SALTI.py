31596b06b0cc9cbd43d79900f0bf26a3e8afcfa2
vdBerg93 <anonymous@a.com>
2021-03-04 15:13:35 +0100

    cleaned

--- SALTI src/main_SALTI.py
+++ SALTI src/main_SALTI.py
@@ -18,8 +18,6 @@ def label_single():
     Then it runs the rgb yolo detection, which returns detection for single image
     :return:
     '''
-
-
     #RESIZE THE PICTURES
     #resize_and_save_image(dir_thermal,dir_thermal_resized,"I00000.jpg",416, 256)
     # dir_thermal_test_image = r"Data\Dataset_V0\images\set00\V000\thermal_resized\I00000.jpg"
@@ -39,7 +37,7 @@ def label_single():
     draw_bboxs(img_C, boxes_C, confs_C, classes_C, classnames_RGB)
     cv2.imshow("RGB YOLO", img_C)
 
-    # TEST THERMAL YOLO
+    # LABEL THERMAL IMAGE
     net_T, classnames_T, opt, device = nettherm.initialize()
     boxes_T, confs_T, classes_T = nettherm.detect(net_T, img_T, opt, device)
 
@@ -47,35 +45,24 @@ def label_single():
     draw_bboxs(img_T, boxes_T, confs_T, classes_T, classnames_RGB)
     cv2.imshow("THERMAL", img_T)
 
-    #nettherm.detect_old()
-
-    print("Insert thermal here")
-
-    # TEST MERGING
+    # Merge the labels
     assert(type(boxes_C)==type(boxes_T))
+    assert(type(confs_T)==type(confs_C))
+    assert(type(classes_T)==type(classes_C))
     boxes, classes, confs = nms(boxes_C + boxes_T, confs_C+confs_T, classes_C+classes_T, cfg_T.confThreshold, cfg_T.nmsThreshold)
-
-    #Exporting the results
-    df=save_objects(r"Data\Dataset_V0\images\set00\V000\thermal", "I00000", ".jpg", boxes, confs, classes, classnames_T, 640, 512)
 
     # Add Bounding Boxes to image
     draw_bboxs(img_M, boxes, confs, classes, classnames_RGB)
     cv2.imshow("MERGED", img_M)
 
-    #(boxes, confidences, classes, conf_threshold=0.5, nms_threshold=0.3 ):
-
-    # det_merged = mergefunction(...)
-    # merged labels = nms(...)
-    print("Insert merging")
-
-    # Exporting
+    #Exporting the results
+    #df=save_objects(r"Data\Dataset_V0\images\set00\V000\thermal", "I00000", ".jpg", boxes, confs, classes, classnames_T, 640, 512)
 
     # Wait until finished
-    # Wait until finished
+    input("Press Enter to finish test...")
     key = cv2.waitKey(0)
     if key == 27:
         cv2.destroyAllWindows()
-    input("Press Enter to finish test...")
 
 label_single()
 
