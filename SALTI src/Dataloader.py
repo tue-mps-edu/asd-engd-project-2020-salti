870756b75734c73e8594af98a020941e70cfa9d9
vdBerg93 <anonymous@a.com>
2021-03-08 15:49:06 +0100

    small fix

--- SALTI src/Dataloader.py
+++ SALTI src/Dataloader.py
@@ -6,14 +6,12 @@ input_formats = ['.png','.jpg','.jpeg']
 
 
 class Dataloader():
-    def __init__(self, path_rgb, path_thermal, output_size=[-1,-1], resize=False, debug=False):
+    def __init__(self, path_rgb, path_thermal, output_size=[-1,-1], debug=False):
         self.DEBUG = debug                      # Debug flag for skipping images
         self.count = 0                          # Logging the current image
         self.progress = 0                       # for progress bar
         self.path_rgb = path_rgb                # path to rgb directory
         self.path_thermal = path_thermal        # path to thermal directory
-        self.resize_images = resize             # BOOLEAN whether images should be resized
-        self.image_output_size = output_size    # Output size of the image
 
         # Only directories are allowed as input
         assert(os.path.isdir(path_rgb) and os.path.isdir(path_thermal))
@@ -47,10 +45,6 @@ class Dataloader():
             img_C = cv2.imread(path_C)                      # Read color image
             img_T = cv2.imread(path_T)                      # Read thermal image
 
-            if self.resize_images:
-                img_C = resize_image(img_C, self.image_output_size)
-                img_T = resize_image(img_T, self.image_output_size)
-
             self.count = self.count + 1                     # Update to next image
             self.progress = (self.count/self.nr_imgs)*100   # Update progress
             yield img_C, img_T
@@ -58,7 +52,7 @@ class Dataloader():
 def test_dataloader():
     path_t = r'D:\KAIST\set00\V000\lwir'
     path_c = r'D:\KAIST\set00\V000\visible'
-    data = Dataloader(path_c, path_t, output_size=[320,320], resize=True, debug=False)
+    data = Dataloader(path_c, path_t, output_size=[320,320], debug=True)
     for img_c, img_t in data:
         cv2.imshow("rgb",img_c)
         cv2.imshow("thermal",img_t)
