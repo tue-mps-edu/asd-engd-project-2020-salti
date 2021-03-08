072a19563a646651e6ffb3fe0f207d943c47c635
Iyer <v.k.iyer@tue.nl>
2021-03-08 10:34:26 +0100

    Edited the save function + Changes in thresholds

--- SALTI src/detect_thermal.py
+++ SALTI src/detect_thermal.py
@@ -19,8 +19,8 @@ def initialize():
     parser.add_argument('--source', type=str, default=r'C:\Github\asd-pdeng-project-2020-developer\SALTI src\Data\mytestdata\RGB', help='source')  # input file/folder, 0 for webcam
     parser.add_argument('--output', type=str, default='output', help='output folder')  # output folder
     parser.add_argument('--img-size', type=int, default=416, help='inference size (pixels)')
-    parser.add_argument('--conf-thres', type=float, default=0.3, help='object confidence threshold')
-    parser.add_argument('--nms-thres', type=float, default=0.5, help='iou threshold for non-maximum suppression')
+    parser.add_argument('--conf-thres', type=float, default=cfg_T.confThreshold, help='object confidence threshold')
+    parser.add_argument('--nms-thres', type=float, default=cfg_T.nmsThreshold, help='iou threshold for non-maximum suppression')
     parser.add_argument('--fourcc', type=str, default='mp4v', help='output video codec (verify ffmpeg support)')
     parser.add_argument('--half', action='store_true', help='half precision FP16 inference')
     parser.add_argument('--device', default='', help='device id (i.e. 0 or 0,1) or cpu')
--- SALTI src/main_SALTI.py
+++ SALTI src/main_SALTI.py
@@ -18,16 +18,17 @@ def label_loop(image_path):
     net_RGB, classnames_RGB = netrgb.initialize()
     net_T, classnames_T, opt, device = nettherm.initialize()
 
-    df_whole = create_archive()
+    # df_whole = create_archive()
 
     i = 0
     for filename_rgb in os.listdir(dir_rgb):
         i = i+1
-        if i%200!=0:
+        if i%50!=0:
             continue
         for filename_thermal in os.listdir(dir_thermal):
             if filename_thermal == filename_rgb:
                 # Bookkeeping
+
                 file_name = os.path.splitext(filename_thermal)[0]
                 file_ext = os.path.splitext(filename_thermal)[1]
 
@@ -57,12 +58,16 @@ def label_loop(image_path):
                                             cfg_T.confThreshold, cfg_T.nmsThreshold)
 
                 # Exporting the results
-                df = save_objects(dir_thermal, file_name, file_ext, boxes, confs, classes,classnames_T, output_width, output_height)
+                classes = [int(i) for i in classes]
+                df = save_objects(dir_Validation, file_name, file_ext, boxes, confs, classes,classnames_T, output_width, output_height)
+                # df_whole = df_whole.append(pd.Series(0, index=df_whole.columns), ignore_index=True)
+                cv2.imwrite(os.path.join(dir_Validation, file_name+file_ext), img_M)
 
                 # Add Bounding Boxes to image
                 draw_bboxs(img_M, boxes, confs, classes, classnames_RGB)
                 cv2.imshow("MERGED", img_M)
-                cv2.waitKey(1)
+                key = cv2.waitKey(10)
+
 
 label_loop(dir_dataset)
 
@@ -79,72 +84,74 @@ label_loop(dir_dataset)
         #img_thermal = cv2.imread(os.path.join(dir_thermal_resized,file_name+file_ext))
 '''
 
-def label_single(rgb_im,thermal_im):
-    '''
-    Test script for labelling a single image
-    it runs the RGB yolo deteection, which returns detection for single image
-    Then it runs the rgb yolo detection, which returns detection for single image
-    :return:
-    '''
-
-
-    #RESIZE THE PICTURES
-    #resize_and_save_image(dir_thermal,dir_thermal_resized,"I00000.jpg",416, 256)
-    # dir_thermal_test_image = r"Data\Dataset_V0\images\set00\V000\thermal_resized\I00000.jpg"
-
-    # TEST RGB YOLO
-    dir_rgb_test_image = rgb_im
-    dir_thermal_test_image = thermal_im
-
-    img_C = cv2.imread(dir_rgb_test_image)
-    img_T = cv2.imread(dir_thermal_test_image)
-    img_M = img_T.copy()
-
-    net_RGB, classnames_RGB = netrgb.initialize()
-    boxes_C, confs_C, classes_C = netrgb.detect(net_RGB, classnames_RGB, img_C)
-
-    # Add Bounding Boxes to image
-    draw_bboxs(img_C, boxes_C, confs_C, classes_C, classnames_RGB)
-    cv2.imshow("RGB YOLO", img_C)
-
-    # TEST THERMAL YOLO
-    net_T, classnames_T, opt, device = nettherm.initialize()
-    boxes_T, confs_T, classes_T = nettherm.detect(net_T, img_T, opt, device)
-
-    # Add Bounding Boxes to image
-    draw_bboxs(img_T, boxes_T, confs_T, classes_T, classnames_RGB)
-    cv2.imshow("THERMAL", img_T)
-
-    #nettherm.detect_old()
-
-    print("Insert thermal here")
-
-    # TEST MERGING
-    assert(type(boxes_C)==type(boxes_T))
-    boxes, classes, confs = nms(boxes_C + boxes_T, confs_C+confs_T, classes_C+classes_T, cfg_T.confThreshold, cfg_T.nmsThreshold)
-
-    #Exporting the results
-    df=save_objects(r"Data\Dataset_V0\images\set00\V000\thermal", "I00000", ".jpg", boxes, confs, classes, classnames_T, 640, 512)
-    # read_and_display_boxes(r"Data\Dataset_V0\images\set00\V000\thermal", "I00000")
-
-
-    # Add Bounding Boxes to image
-    draw_bboxs(img_M, boxes, confs, classes, classnames_RGB)
-    cv2.imshow("MERGED", img_M)
-
-    #(boxes, confidences, classes, conf_threshold=0.5, nms_threshold=0.3 ):
-
-    # det_merged = mergefunction(...)
-    # merged labels = nms(...)
-    print("Insert merging")
-
-    # Exporting
-
-    # Wait until finished
-    # Wait until finished
-    key = cv2.waitKey(0)
-    if key == 27:
-        cv2.destroyAllWindows()
-    input("Press Enter to finish test...")
-
-# label_single()
+# def label_single(rgb_im,thermal_im):
+#     '''
+#     Test script for labelling a single image
+#     it runs the RGB yolo deteection, which returns detection for single image
+#     Then it runs the rgb yolo detection, which returns detection for single image
+#     :return:
+#     '''
+#
+#
+#     #RESIZE THE PICTURES
+#     #resize_and_save_image(dir_thermal,dir_thermal_resized,"I00000.jpg",416, 256)
+#     # dir_thermal_test_image = r"Data\Dataset_V0\images\set00\V000\thermal_resized\I00000.jpg"
+#
+#     # TEST RGB YOLO
+#     dir_rgb_test_image = rgb_im
+#     dir_thermal_test_image = thermal_im
+#
+#     img_C = cv2.imread(dir_rgb_test_image)
+#     img_T = cv2.imread(dir_thermal_test_image)
+#     img_M = img_T.copy()
+#
+#     net_RGB, classnames_RGB = netrgb.initialize()
+#     boxes_C, confs_C, classes_C = netrgb.detect(net_RGB, classnames_RGB, img_C)
+#
+#     # Add Bounding Boxes to image
+#     draw_bboxs(img_C, boxes_C, confs_C, classes_C, classnames_RGB)
+#     cv2.imshow("RGB YOLO", img_C)
+#
+#     # TEST THERMAL YOLO
+#     net_T, classnames_T, opt, device = nettherm.initialize()
+#     boxes_T, confs_T, classes_T = nettherm.detect(net_T, img_T, opt, device)
+#
+#     # Add Bounding Boxes to image
+#     draw_bboxs(img_T, boxes_T, confs_T, classes_T, classnames_RGB)
+#     cv2.imshow("THERMAL", img_T)
+#
+#     #nettherm.detect_old()
+#
+#     print("Insert thermal here")
+#
+#     # TEST MERGING
+#     assert(type(boxes_C)==type(boxes_T))
+#     boxes, classes, confs = nms(boxes_C + boxes_T, confs_C+confs_T, classes_C+classes_T, cfg_T.confThreshold, cfg_T.nmsThreshold)
+#     # boxes, classes, confs = nms(boxes_C + boxes_T, confs_C+confs_T, classes_C+classes_T, 0.2, cfg_T.nmsThreshold)
+#
+#
+#     #Exporting the results
+#     df=save_objects(r"Data\Dataset_V0\images\set00\V000\thermal", "I00000", ".jpg", boxes, confs, classes, classnames_T, 640, 512)
+#     # read_and_display_boxes(r"Data\Dataset_V0\images\set00\V000\thermal", "I00000")
+#
+#
+#     # Add Bounding Boxes to image
+#     draw_bboxs(img_M, boxes, confs, classes, classnames_RGB)
+#     cv2.imshow("MERGED", img_M)
+#
+#     #(boxes, confidences, classes, conf_threshold=0.5, nms_threshold=0.3 ):
+#
+#     # det_merged = mergefunction(...)
+#     # merged labels = nms(...)
+#     print("Insert merging")
+#
+#     # Exporting
+#
+#     # Wait until finished
+#     # Wait until finished
+#     key = cv2.waitKey(0)
+#     if key == 27:
+#         cv2.destroyAllWindows()
+#     input("Press Enter to finish test...")
+#
+# label_single(r'D:\Courses\Block 2\Inhouse-project\GIT\asd-pdeng-project-2020-developer\SALTI src\Data\set03_V001\images\set03\V001\visible\I00199.jpg',r'D:\Courses\Block 2\Inhouse-project\GIT\asd-pdeng-project-2020-developer\SALTI src\Data\set03_V001\images\set03\V001\lwir\I00199.jpg')
