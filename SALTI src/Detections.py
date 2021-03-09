9ccd05b9b9013e12330a69616c1ff226a9cf1c8b
vdBerg93 <anonymous@a.com>
2021-03-09 15:16:39 +0100

    update test funtion for merger

--- SALTI src/Detections.py
+++ SALTI src/Detections.py
@@ -20,5 +20,11 @@ def add_classes(a, b):
 def test_detections():
     a=Detections([1,2],[3,4],[5,6])
     b=Detections([11,122],[13,14],[15,16])
+    L_before = len(a.confidences)
     c=a.append(b)
+    print(a)
     print(c)
+    assert(len(a.confidences)==L_before)
+
+
+test_detections()
