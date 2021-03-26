220e2010a6bd6c0bb51fbc7241a58d1f41788b0f
vdBerg93 <anonymous@a.com>
2021-03-26 15:32:41 +0100

    Comment merger

--- SALTI/Merger.py
+++ SALTI/Merger.py
@@ -3,20 +3,30 @@ import cv2
 
 class Merger():
     def __init__(self,conf_threshold,nms_threshold):
+        ''' Merger (class) that facilitates the filtering of detections
+        argument 1: (float) confidence threshold
+        argument 2: (float) NMS threshold
+        '''
         self.confThreshold=conf_threshold
         self.nmsThreshold=nms_threshold
 
-    def merge(self, detections):
-        return self.confidence_filter(detections)
-
     def confidence_filter(self,detections):
+        '''Filtering detections based on a confidence threshold
+        argument 1: (Detections) input detections
+        return 1: (Detections) filtered detections
+        '''
         to_keep = [i for i,conf in enumerate(detections.confidences) if conf>self.confThreshold]
         return Detections([detections.boxes[i] for i in to_keep],
                           [detections.classes[i] for i in to_keep],
                           [detections.confidences[i] for i in to_keep])
 
     def NMS(self, detections):
-        # Non maximum suppression, will give indices to keep
+        '''
+        Non-maximum suppression of bounding boxes
+        argument 1: (Detections) input detections
+        return 1: (Detections) filtered detections
+        '''
+        #Non maximum suppression, will give indices to keep
         indices = cv2.dnn.NMSBoxes(detections.boxes, detections.confidences, self.confThreshold, self.nmsThreshold)
         to_keep = [i[0] for i in indices]
         return Detections([detections.boxes[i] for i in to_keep],
