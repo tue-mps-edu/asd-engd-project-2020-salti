272fed20cd312781769e6be93a36600bca1b41a6
vdBerg93 <anonymous@a.com>
2021-03-26 15:36:50 +0100

    comment preprocessor

--- SALTI/Preprocesser.py
+++ SALTI/Preprocesser.py
@@ -2,12 +2,22 @@ import cv2
 import numpy as np
 
 class Preprocessor():
-
     def __init__(self,output_size=[640,512],  enhancing=False):
+        '''
+        Class for preprocessing images
+        Argument 1 (list): image output size
+        Argument 2 (bool): trigger image filtering/enhancing
+        '''
         self.output_size = output_size  # [x,y]
         self.do_enhancing = enhancing
 
     def process(self, img):
+        '''
+        Preprocess an image
+        Argument (opencv img): image to process
+        Return (opencv img): preprocessed image
+        '''
+
         self.do_resize = not(img.shape[0] == self.output_size[0] and img.shape[1] == self.output_size[1])
         if self.do_resize:
             img = self.resize_image(img)
@@ -18,18 +28,28 @@ class Preprocessor():
             return img, img
 
     def resize_image(self, img_in):
+        '''
+        Resize a single image
+        Argument (opencv image): input image
+        Returns (opencv image): resized image
+        '''
         return cv2.resize(img_in, (self.output_size[0],self.output_size[1]), interpolation=cv2.INTER_AREA)
 
     def add_enhancing(self, img_in):
+        '''
+        Enhance an image by filtering it
+        Argument (opencv image): input image
+        Returns (opencv image): resized image
+        '''
 
-        # Determine average intensity level of the image
         def find_energy(img_gray):
+            '''Determine average intensity level of the image'''
             img_gray = img_gray.astype('float64')
             energy = np.sqrt(np.mean(img_gray ** 2))
             return energy
 
-        # Sharpen based on the normalized image intensity level
         def adaptive_sharpen(img_gray_received):
+            '''Sharpen based on the normalized image intensity level'''
             img_sharpened = img_gray_received.copy() * 1.0
             blurred = cv2.GaussianBlur(img_sharpened, (5, 5), 2) * 1.0
             img_sharpened = 2.0 * img_sharpened - 1.6 * blurred
