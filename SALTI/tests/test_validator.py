1769010d3b0186e2f1abff6cff9c4ceb4973072d
vdBerg93 <anonymous@a.com>
2021-03-26 17:28:41 +0100

    fix validator tests

--- SALTI/tests/test_validator.py
+++ SALTI/tests/test_validator.py
@@ -1,8 +1,9 @@
 import SALTI
 
 def test_validator():
-    results_directory=r'C:\Users\20204916\Documents\GitHub\asd-pdeng-project-2020-developer\SALTI\Output\2021.03.14_18h27m52s'
+    print("Update the path & labeltype in test_validator before running!")
+    results_directory=r'C:\Github\asd-pdeng-project-2020-developer\test_images\outputs\2021.03.26_12h16m40s'
     image_ext='.jpg'
     iou_threshold=0.8
-    v = SALTI.Validator(results_directory,image_ext,iou_threshold)
+    v = SALTI.Validator(results_directory,image_ext,iou_threshold, 'PaSCalVOC')
     v.complete_Validation()
